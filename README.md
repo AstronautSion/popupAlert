5445bd00321c524e99ae198b08bbf3e373c2d508
astronaut <astronaut.sion@gmail.com>
2018-09-19 05:14:18 +0900

    md. arguments exception handling

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # popupAlert
 
+수정된 부분
+- PopupAlert arguments의 갯수 3개로 제한
+- arguments의 type에 따라 적용 ( string : 얼럿메세지 ) / ( function : callback ) / ( number : button type )
+
 수정할 부분
-1. 팝업 마크업 클래스명 커스텀 할 수 있도록..
-2. 버튼 경우의 수 : 
-    - 취소, 확인(callback) 
-    - 확인(callback) 
-    - 확인  
-    (3가지 경우의 수로 수정하기)
+1. 버튼 경우의 수 :
+    - null : 기본세팅값 (3)
+    - 1    : 버튼 1, 콜백 0
+    - 2    : 버튼 1, 콜백 1
+    - 3    : 버튼 2, 콜백 1 
--- popupAlert.js
+++ popupAlert.js
@@ -1,74 +1,84 @@
-function PopupAlert(msg, callback){
-    this.popupParent    = null;
-    this.popup          = null;
-
-    this.strPopup       = null;
-    this.strBtnConfirm  = null;
-    this.strBtnCancel   = null;
-
-    this.msg            = null;
-    this.cb             = null;
-    this.init(msg, callback);
+function PopupAlert(){
+    
+    this._popupParent    = null;
+    this._popup          = null;
+    this._classNm        = null;
+    this._btnNum         = null;
+    this._msg            = null;
+    this._cb             = null;
+    this.init(arguments);
 }
 PopupAlert.error = function(){
     throw Error('new PopupAlert( string, [ function ]) ::Plz check the Type of arguments');
 }
-
 PopupAlert.prototype = {
+    
+    init : function( arguments){
 
-    init : function(msg, callback){
-        if(typeof msg !== 'string') this.error();
-        if(callback) if(typeof callback !== 'function'){ this.error(); }
-
-        this.popupParent    = document.getElementById('popupAlert');
-        this.popup          = null;
+        if(arguments.lenth > 2 ) throw Error('arguments length too many..');
+        else if(arguments.lenth === 0) throw Error('plz write a letter');
+        var that = this;
+        Array.prototype.slice.call(arguments).forEach(function(a){
+            if( typeof a === 'string' ) that._msg = a; 
+            else if( typeof a === 'fuction') that._cb = a;
+            else if( typeof a === 'number') that._btnNum = a;
+        });
         
-        this.strPopup       = 'popup-alert';
-        this.strBtnConfirm  = 'btn-confirm-popup-alert';
-        this.strBtnCancel   = 'btn-cancel-popup-alert';
-
-        this.msg            = msg;
-        this.cb             = callback;
-
+        this._popupParent    = document.getElementById('popupAlert');
+        this._popup          = null;
+        this._classNm       = {
+            popup       : 'popup-alert',
+            btnConfirm  : 'btn-confirm-popup-alert',
+            btnCancel   : 'btn-cancel-popup-alert'
+        };
+       
         this.initEvent();
         this.createPopup();
         this.createBtnChoice();
         this.popupEvent();
     },
     initEvent: function(){
-        if( document.querySelectorAll('.'+this.strPopup).length === 0 ){
-            this.popupParent.style.display = "block";
+        if( document.querySelectorAll('.'+this._classNm.popup).length === 0 ){
+            this._popupParent.style.display = "block";
         }
     },
     createPopup : function(){
-        var idx = document.querySelectorAll('.'+this.strPopup).length;
-        var html = '<div id="'+this.strPopup+idx+'" class="'+this.strPopup+'">' + 
+        var idx = document.querySelectorAll('.'+this._classNm.popup).length;
+        var html = '<div id="'+this._classNm.popup+idx+'" class="'+this._classNm.popup+'">' + 
                         '<div class="popup-alert-text-area">' + 
-                            '<p class="popup-alert-text">'+this.msg+'</p>' +
+                            '<p class="popup-alert-text">'+this._msg+'</p>' +
                         '</div>' +
                     '</div>';   
-        this.popupParent.insertAdjacentHTML('beforeend', html);
-        this.popup = this.popupParent.querySelector('#'+this.strPopup+idx);
+        this._popupParent.insertAdjacentHTML('beforeend', html);
+        this._popup = this._popupParent.querySelector('#'+this._classNm.popup+idx);
     },
     createBtnChoice : function(){
+        if( this._btnNum == null ){
+            console.log('test');
+        }
+        //null = 기본상태 (2)
+        //1 = 버튼 1개 콜백 0개
+        //2 = 버튼 1개 콜백 1개
+        //3 = 버튼 2개 콜백 1개
+
         var btns = null;
-        if(this.cb){
+        if(this._cb){
             btns =  '<div class="popup-alert-btn-area type1">' + 
-                        '<button type="button" class="'+this.strBtnCancel+'">취소</button>' +
-                        '<button type="button" class="'+this.strBtnConfirm+'">확인</button>' +
+                        '<button type="button" class="'+this._classNm.btnCancel+'">취소</button>' +
+                        '<button type="button" class="'+this._classNm.btnConfirm+'">확인</button>' +
                     '</div>';
         }else{ 
             btns =  '<div class="popup-alert-btn-area type2">' + 
-                        '<button type="button" class="'+this.strBtnConfirm+'">확인</button>' +
+                        '<button type="button" class="'+this._classNm.btnConfirm+'">확인</button>' +
                     '</div>';
         }
-        this.popup.insertAdjacentHTML('beforeend',btns);
+        this._popup.insertAdjacentHTML('beforeend',btns);
     },
     popupEvent : function(){
         var that        = this;
-        var confirmBtn  = this.popup.querySelector('.'+this.strBtnConfirm);
-        var closeBtn    = this.popup.querySelector('.'+this.strBtnCancel);
-        if(this.cb){
+        var confirmBtn  = this._popup.querySelector('.'+this._classNm.btnConfirm);
+        var closeBtn    = this._popup.querySelector('.'+this._classNm.btnCancel);
+        if(this._cb){
             closeBtn.addEventListener('click', function(){ that.closePopupEvent(); });
             confirmBtn.addEventListener('click', function(){ that.confirmPopupEvent(); });
         }else{
@@ -76,18 +86,18 @@ PopupAlert.prototype = {
         }
     },
     hidePopupEvent : function(){
-        if( this.popupParent.querySelectorAll('.'+this.strPopup).length == 0 ){
-            this.popupParent.style.display = 'none';
+        if( this._popupParent.querySelectorAll('.'+this._classNm.popup).length == 0 ){
+            this._popupParent.style.display = 'none';
         }
     },
     closePopupEvent: function(){
-        this.popup.remove();
+        this._popup.remove();
         this.hidePopupEvent();
     },
     confirmPopupEvent: function(){
-        this.popup.remove();
+        this._popup.remove();
         this.hidePopupEvent();
-        this.cb();
+        this._cb();
     }
 }
 
